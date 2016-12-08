fd115ed1123edd2b7a0998320c490a85dd275659
Michal Brašna <michal.brasna@socialbakers.com>
2016-12-08 11:11:01 +0100

    Improve tankovna parser

--- service/scrapper/tankovna.js
+++ service/scrapper/tankovna.js
@@ -11,9 +11,21 @@ module.exports = (config, serviceConfig) => {
     let dailyMenus = obj.daily_menus
     let out = []
     for (let i = 0; i < dailyMenus.length; i++) {
+      let dishes = dailyMenus[i].daily_menu.dishes
+      let matches = dishes[0].dish.name.match(/Polévka\s*(\d+).+?(MENU)\s*(\d+).+?(\d{2,}).+?(\d{2,}).+?(\d{2,}).+?(\d{2,})/i)
+      if (matches && dishes && dishes.length === 6) {
+        dishes[0].dish.name = matches[2]
+        dishes[0].dish.price = matches[3] + ' Kč'
+        dishes[1].dish.price = matches[1] + ' Kč'
+        dishes[2].dish.price = matches[4] + ' Kč'
+        dishes[3].dish.price = matches[5] + ' Kč'
+        dishes[4].dish.price = matches[6] + ' Kč'
+        dishes[5].dish.price = matches[7] + ' Kč'
+      }
+
       out.push({
         date: zo.parseDate(dailyMenus[i].start_date),
-        items: _.map(dailyMenus[i].daily_menu.dishes, zo.mapItem)
+        items: _.map(dishes, zo.mapItem)
       })
     }
 
